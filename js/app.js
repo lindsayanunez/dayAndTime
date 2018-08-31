7afd73519655bb5dc2f88fec86fd64937c439914
Lindsay Nunez <lindsayanunez@gmail.com>
2018-08-31 11:18:51 -0400

    Fixed day to show the day and not the index number.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 let date = new Date();
 let day = date.getDay();  //Get the weekday as a number (0-6)
 let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let dayOfWeek = daysOfWeek[day];
 let hour = date.getHours();
 let minute = date.getMinutes();
 let second = date.getSeconds();
@@ -9,6 +10,6 @@ let second = date.getSeconds();
 
 function displayToday(){
   const todaySpace =document.querySelector('.today');
-  todaySpace.innerHTML = `Today is: ${day}. Current time is : ${hour} AM : ${minute} : ${second}`;
+  todaySpace.innerHTML = `Today is: ${dayofWeek}. Current time is : ${hour} AM : ${minute} : ${second}`;
 }
 
