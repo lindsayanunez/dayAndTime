8257e2e582399451575184619c89637f9bf9ff8b
Lindsay Nunez <lindsayanunez@gmail.com>
2018-08-31 11:16:25 -0400

    Date and time variables added.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 
-//first iteration
+let date = new Date();
+let day = date.getDay();  //Get the weekday as a number (0-6)
+let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+let hour = date.getHours();
+let minute = date.getMinutes();
+let second = date.getSeconds();
 
-function displayToday =>{
+
+function displayToday(){
   const todaySpace =document.querySelector('.today');
   todaySpace.innerHTML = `Today is: Friday. Current Time is: 10 AM : 30 : 10`;
 }
