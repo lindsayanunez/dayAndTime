333b28f6772ae5975bd115a370dc4759a1992623
Lindsay Nunez <lindsayanunez@gmail.com>
2018-08-31 11:14:44 -0400

    first iteration of function to display text in the html.

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+//first iteration
+
+function displayToday =>{
+  const todaySpace =document.querySelector('.today');
+  todaySpace.innerHTML = `Today is: Friday. Current Time is: 10 AM : 30 : 10`;
+}
+
