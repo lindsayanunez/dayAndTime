fc15812c4ec5f6284190273f5b03e440edbd34d6
Lindsay Nunez <lindsayanunez@gmail.com>
2018-08-31 11:19:48 -0400

    Changed hour so it is no longer military time.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let date = new Date();
 let day = date.getDay();  //Get the weekday as a number (0-6)
 let daysOfWeek = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 let dayOfWeek = daysOfWeek[day];
-let hour = date.getHours();
+let hour = (date.getHours()> 12? (date.getHours()- 12):date.getHours());
 let minute = date.getMinutes();
 let second = date.getSeconds();
 
